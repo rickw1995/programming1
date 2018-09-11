81c7b848c2d97065fb482c34db904afe27eefeca
rickw1995 <rickweggemans@live.nl>
2018-09-11 18:03:29 +0200

    opdracht 3 en 4

---
+++ 3.statements.py
@@ -0,0 +1,27 @@
+#3.1
+a = 6
+b = 7
+
+#3.2
+c = (a+b)/2
+
+#3.3
+voornaam = 'Rick '
+tussenvoegsel = ''
+achternaam = 'Weggemans'\
+
+#3.4
+mijnnaam = voornaam + achternaam
+
+#4.1
+75 > a
+75 < b
+
+#4.2
+(len(mijnnaam) == (len(voornaam + achternaam)))
+
+#4.3
+(len(mijnnaam) / 5) == len(tussenvoegsel)
+
+#4.4
+tussenvoegsel in achternaam
