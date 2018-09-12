c915386c852f7f27e76795fe8f059ec5b701dc6b
rickw1995 <rickweggemans@live.nl>
2018-09-12 08:23:00 +0200

    python lists

---
+++ 1.lists&strings.py
@@ -0,0 +1,10 @@
+favorieten = ['Eminem']
+
+favorieten.append('Kanye west')
+
+print(favorieten)
+
+favorieten[1] = 'shotgun'
+
+print(favorieten)
+
---
+++ 2.lists&numbers.py
@@ -0,0 +1,3 @@
+list = [3, 7, -2, 12]
+
+print('Het bereik van deze lijst is ' + str(max(list) - min(list)))
