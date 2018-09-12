22bf44850497558de80b1dd146d0e317f7669b65
rickw1995 <rickweggemans@live.nl>
2018-09-12 17:10:57 +0200

    basis concepten

---
+++ 3.input/output.py
@@ -0,0 +1,6 @@
+uurloon = float(input('Hoeveel Euro verdien je per uur? '))
+Worked = int(input('Hoeveel uur heb je gewerkt? '))
+
+Verdiend = float(uurloon * Worked)
+
+print(str(Worked) + (' Uur werken levert ') + str(Verdiend) + (' Euro op.'))
