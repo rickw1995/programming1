f5ef33e9203aa8b26d7c0ee52c8b59ba8b6c3971
rickw1995 <rickweggemans@live.nl>
2018-09-12 17:11:47 +0200

    lists

---
+++ 3.Tuples.py
@@ -0,0 +1,7 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+list = []
+list.append((letters.count('A')))
+list.append((letters.count('B')))
+list.append((letters.count('C')))
+
+print(list)
